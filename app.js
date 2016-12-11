7d7e542077b4aca1f7c2d7fd084dd49e0ec3736c
kbazant <krizz100@yahoo.com>
2016-12-11 23:28:01 +0100

    added const for text input

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
 
 myButton.addEventListener('click', () => { 
 	myHeading.style.color = 'red';
