d64e656adc3ac40a15c3681cec5eafa76178e0cd
kbazant <krizz100@yahoo.com>
2016-12-14 23:48:30 +0000

    changed certain list elements colour by selecting them by class name

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const myList = document.getElementByTagName('li');
+const myList = document.getElementsByTagName('li');
 
-for  (let i = 0, i < myList.length; i += 1) {
+for (let i = 0; i < myList.length; i += 1) {
 	myList[i].style.color = 'purple';
+}
+
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+	errorNotPurple[i].style.color = 'red';
 }
