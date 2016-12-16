2d6d4b86e59530d3701057156f2db58c9d572b6f
kbazant <krizz100@yahoo.com>
2016-12-16 01:12:39 +0000

    replaced "textContent" with "innerHTML"

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const p = document.querySelector('p.description');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-	p.textContent = input.value + ':';
+	p.innerHTML = input.value + ':';
 })
