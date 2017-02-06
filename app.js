e884fd989a60539abe724140902ebda76f9b601c
kbazant <krizz100@yahoo.com>
2017-02-06 02:11:01 +0000

    Used previousElementSibling and insertBefore

--- app.js
+++ app.js
@@ -9,11 +9,24 @@ const addItemButton = document.querySelector('button.addItemButton');
 
 listUl.addEventListener('click', (event) => {
 	if (event.target.tagName == 'BUTTON') {
-		let li = event.target.parentNode;
-		let ul = li.parentNode;
-		ul.removeChild(li);
+		if (event.target.className == 'remove') {
+			let li = event.target.parentNode;
+			let ul = li.parentNode;
+			ul.removeChild(li);
+		}
+
+
+		if (event.target.className == 'up') {
+			let li = event.target.parentNode;
+			let prevLi = li.previousElementSibling;
+			let ul = li.parentNode;
+			if (prevLi) {
+				ul.insertBefore(li, prevLi);
+			}
+		}
 	}
 });
+
 
 toggleList.addEventListener('click', () => {
 	if (listDiv.style.display == 'none') {
@@ -29,4 +42,3 @@ descriptionButton.addEventListener('click', () => {
 	descriptionP.innerHTML = descriptionInput.value + ':';
 	descriptionInput.value = '';
 });
-
